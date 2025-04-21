2390bfed680671e4fe40cf59fb53c79ba8cf81dd
luchovs <lucianosavia15@gmail.com>
2025-04-21 16:49:56 -0300

    Hicimos la introducción a javascript

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+
+
+
+//variables
+var miVariable = "hola soy una variable con var"; // no se usa
+let miVariable2 = "variable con let"; // solo vive con el scope declarado
+const miVariable3 = "constante no cambian los valores";
+
+const hola = "hola fede";
+console.log(hola) // es el equivalente a print
