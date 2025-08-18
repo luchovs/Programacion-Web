e0858d2b1ced884b58447d869f6f959ab73e6dba
luchovs <lucianosavia15@gmail.com>
2025-08-18 16:56:04 -0300

    Aprendimos Props Drillings y React

---
+++ react-context/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './css/App.css'
+import { Persona } from "./components/persona";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <Persona name={"Luciano"} />
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ react-context/src/components/persona2.jsx
@@ -0,0 +1,8 @@
+export function Persona2({ name }) {
+    return (
+    <div>
+        <h2>persona 2</h2>
+        <p>hola {name}</p>
+    </div>
+    );
+}
